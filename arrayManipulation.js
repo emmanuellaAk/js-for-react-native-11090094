536a691f8d703d35715e02f653ef2c6ae6b8fcfc
emmanuellaAk <emmanuellaanimwaa17@icloud.com>
2024-05-18 21:19:26 +0000

    end of task 1

---
+++ arrayManipulation.js
@@ -0,0 +1,13 @@
+function processArray(numbers) {
+  return numbers.map(num => {
+      if (num % 2 === 0) {
+          return num * num; 
+      } else {
+          return num * 3;   
+      }
+  });
+}
+
+const inputArray = [1, 2, 3, 4, 5];
+const processedArray = processArray(inputArray);
+console.log(processedArray);
