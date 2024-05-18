002842d4a23a9549e4473bfc2c0a8e4ce8cad084
emmanuellaAk <emmanuellaanimwaa17@icloud.com>
2024-05-18 21:43:01 +0000

    end of task 2

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -1,13 +1,36 @@
 function processArray(numbers) {
   return numbers.map(num => {
-      if (num % 2 === 0) {
-          return num * num; 
-      } else {
-          return num * 3;   
-      }
+    if (num % 2 === 0) {
+      return num * num; 
+    } else {
+      return num * 3;   
+    }
   });
+}
+
+function formatArrayStrings(strings, numbers) {
+  
+  if (strings.length !== numbers.length) {
+    throw new Error("Both arrays must have the same length");
+  }
+
+  for (let i = 0; i < strings.length; i++) {
+    if (numbers[i] % 2 === 0) {
+  
+      strings[i] = strings[i].toUpperCase();
+    } else {
+    
+      strings[i] = strings[i].toLowerCase();
+    }
+  }
+
+  return strings;
 }
 
 const inputArray = [1, 2, 3, 4, 5];
 const processedArray = processArray(inputArray);
-console.log(processedArray);
+const stringsArray = ["today", "is", "a", "great", "day"];
+const formattedStrings = formatArrayStrings(stringsArray, processedArray);
+
+console.log(processedArray); 
+console.log(formattedStrings); 
